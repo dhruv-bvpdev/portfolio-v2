802388495a42388bc5a8d1491da35b72ee39efd1
Dhruv Gursahani <dhruv.gursahani@outlook.com>
2022-04-28 21:06:32 +0530

    feat: :sparkles: theme toggle button animation

--- components/theme-toggle-button.js
+++ components/theme-toggle-button.js
@@ -6,12 +6,23 @@ const ToggleThemeButton = () => {
   const { toggleColorMode } = useColorMode()
 
   return (
-    <IconButton
-      aria-label="Toggle Theme"
-      colorScheme={useColorModeValue('purple', 'orange')}
-      icon={useColorModeValue(<MoonIcon />, <SunIcon />)}
-      onClick={toggleColorMode}
-    ></IconButton>
+    <AnimatePresence initial={false}>
+      <motion.div
+        style={{ display: 'inline-block' }}
+        key={useColorModeValue('light', 'dark')}
+        initial={{ y: -20, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        exit={{ y: 20, opacity: 0 }}
+        transition={{ duration: 0.2 }}
+      >
+        <IconButton
+          aria-label="Toggle theme"
+          colorScheme={useColorModeValue('purple', 'orange')}
+          icon={useColorModeValue(<MoonIcon />, <SunIcon />)}
+          onClick={toggleColorMode}
+        ></IconButton>
+      </motion.div>
+    </AnimatePresence>
   )
 }
 
