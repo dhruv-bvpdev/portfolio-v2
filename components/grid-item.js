87b46e6ffd85d6b72b447f0c157e7b83b1094844
Dhruv Gursahani <dhruv.gursahani@outlook.com>
2022-06-05 11:13:53 +0530

    feat: :zap: changed back to next/image in grid-item

--- components/grid-item.js
+++ components/grid-item.js
@@ -1,6 +1,26 @@
 import NextLink from 'next/link'
-import { Box, Text, LinkBox, LinkOverlay, Image } from '@chakra-ui/react'
+import Image from 'next/image'
+import { Box, Text, LinkBox, LinkOverlay } from '@chakra-ui/react'
 import { Global } from '@emotion/react'
+
+const shimmer = (w, h) => `
+<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <linearGradient id="g">
+      <stop stop-color="#333" offset="20%" />
+      <stop stop-color="#222" offset="50%" />
+      <stop stop-color="#333" offset="70%" />
+    </linearGradient>
+  </defs>
+  <rect width="${w}" height="${h}" fill="#333" />
+  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+</svg>`
+
+const toBase64 = str =>
+  typeof window === 'undefined'
+    ? Buffer.from(str).toString('base64')
+    : window.btoa(str)
 
 export const GridItem = ({ children, href, title, thumbnail }) => {
   return (
@@ -31,7 +51,12 @@ export const WorkGridItem = ({ children, id, title, thumbnail }) => {
             src={thumbnail}
             alt={title}
             className="grid-item-thumbnail"
-            w="full"
+            placeholder="blur"
+            blurDataURL={`data:image/svg+xml;base64,${toBase64(
+              shimmer(1080, 1080)
+            )}`}
+            width="1080"
+            height="1080"
           />
           <LinkOverlay href={`/works/${id}`}>
             <Text mt={2} fontSize={20}>
